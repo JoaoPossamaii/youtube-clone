87e4fd08580b349896b2099eaef1b5b30069ae1d
JoaoPossamaii <jpossamai92@hotmail.com>
2023-10-04 18:16:27 -0300

    feat: folders of components and files

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+   <header>
+    <p> Youtube </p>
+   </header>
+  )
+}
+
+export default Header;
---
+++ src/components/player/Player.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Player() {
+
+  const selectedMovie = categories[0].movies[0];
+  const isFavorite = false;
+
+
+  return (
+    <div className='player'>
+      {selectedMovie && (
+        <>
+          <div className='video-container'>
+            <iframe width="560" height="315"
+              src="https://www.youtube.com/embed/O0KVFmrAfB0?si=7M49BmggrASd1gBR" title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+          <div className='movie-details'>
+            <div className='movie-infos'>
+              <h2> {selectedMovie.title}</h2>
+              <p> Released in {selectedMovie.released}</p>
+              <p className='category'> {selectedMovie.title}</p>
+            </div>
+            <div>
+              <button
+                type='button'
+                onClick={() => console.log(selectedMovie)}
+              >
+                {isFavorite ? ' remove from favorite' : 'add form favorite'}
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+export default Player;
+
