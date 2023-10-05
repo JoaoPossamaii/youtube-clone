fb37a14ba8b081c1d5ed52db7954670a3b9fc3d1
JoaoPossamaii <jpossamai92@hotmail.com>
2023-10-05 17:20:18 -0300

    feat: switch case to add movies in favorites

--- src/redux/reducers/favoritesMovies.ts
+++ src/redux/reducers/favoritesMovies.ts
@@ -1,14 +1,31 @@
 import { AnyAction } from "redux";
-import { favoritesReducerState } from "../../types";
+import { FavoritesReducerState, } from "../../types";
+import { UPDATE_FAVORITE_MOVIES } from "../actions/actionIndex";
 
 const INITIAL_STATE_FAV = {
   favoritesMovies: [],
 };
 
-const favoriteReducer = (
+const favoritesReducer = (
   state: FavoritesReducerState = INITIAL_STATE_FAV,
   action: AnyAction,
-) => state;
+) => {
+  switch (action.type) {
+    case UPDATE_FAVORITE_MOVIES: {
+      if (state.favoriteMovies.includes(action.payload.movieId))
+        return {
+          ...state,
+          favoriteMovies: state.favoriteMovies.filter((movie) => movie !== action.payload.movieId)
+
+        }
+      return {
+        ...state,
+        favoriteMovies: [...state.favoriteMovies, action.payload.movieId]
+      }
+    }
+    default: return state;
+  }
+};
 
 
 export default favoritesReducer; 
