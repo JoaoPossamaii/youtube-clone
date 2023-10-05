28b4980011e82a8fca024a703b7dc372342db049
JoaoPossamaii <jpossamai92@hotmail.com>
2023-10-05 08:55:53 -0300

    feat: file sideBar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,32 @@
+import categoriesData from '../../../data/DataIndex'
+
+
 function SideBar() {
+
+  const categories = [...categoriesData];
+
   return (
-    <div>Sidebar</div>
-  )
+    <aside className='sidebar'>
+      {categories.map((category) => (
+        <div className='category-list'>
+          <h3>{category.name}</h3>
+          <ul>
+            {category.movies.map((movie) => (
+              <li>
+                <button
+                  type='button'
+                  onClick={() => console.log(movie, category)}
+                >
+                  {movie.title}
+                  {movie.released}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </aside>
+  );
 }
 
-export default SideBar
+export default SideBar;
